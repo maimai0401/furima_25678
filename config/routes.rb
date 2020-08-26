998f21ab481d77cad57bb824afb5ce4a1a7de866
maimai0401 <64880970+maimai0401@users.noreply.github.com>
2020-08-26 22:55:26 +0900

    ViewとControllerのカスタマイズ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "user#index"
+  root to: "users/registrations#new"
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
+  end
 end
