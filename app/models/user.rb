383f5f6858a6bbdeea65c7ade5ec8e368a202ba1
maimai0401 <64880970+maimai0401@users.noreply.github.com>
2020-08-28 17:05:31 +0900

    バリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
-  validates :email, uniqueness: true, format: { with: /\A\S+@\S+\.\S+\z/ }
   validates :password, length: { minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
   validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :first_name_kana, :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
