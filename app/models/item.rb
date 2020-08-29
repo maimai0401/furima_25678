cd8313adb14bfba70e6c63439d82f6acd48bb533
maimai0401 <64880970+maimai0401@users.noreply.github.com>
2020-08-29 14:38:13 +0900

    各カテゴリー選択実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :genre
+  belongs_to_active_hash :category, :status, :postage, :prefecture, :day
 
   validates :name, :explanation, :category_id, :status_id, :postage_id, :prefecture_id, :days_id, :price, presence: true
   validates :price, numericality:{greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
-  validates :genre_id, numericality: { other_than: 1 } 
+  validates :genre_id, :status_id, :status_id, :prefecture_id, :days_id, numericality: { other_than: 1 } 
 
   belongs_to :user
 end
