900736bf815fc9567df7fce259a66975edcb5ea0
maimai0401 <64880970+maimai0401@users.noreply.github.com>
2020-09-02 12:34:21 +0900

    itemモデルテスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {'test'}
+    explanation {'説明文'}
+    category_id { 2 }
+    status_id { 2 }
+    postage_id { 2 }
+    prefecture_id { 2 }
+    days_id { 2 }
+    price { 1000 }
+    association :user
+
+    # after(:build) { |item| item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png', content_type: 'image/png') }
+
+      
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload("public/images/test_image.png")
+  end
+
+  describe '商品出品' do
+    context '商品出品できる時' do
+      it '全部入力されている時' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品出品できない時' do
+      it '画像が空の時' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が空の時' do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it '商品説明が空の時' do
+        @item.explanation = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+      it 'カテゴリー情報が空の時' do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品状態が空の時' do
+        @item.status_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")      
+      end
+      it '配送料情報が空の時' do
+        @item.postage_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage can't be blank")
+      end
+      it '配送元情報が空の時' do
+        @item.prefecture_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '発送までの日数が空の時' do
+        @item.days_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days can't be blank")
+      end
+      it '価格が空の時' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '価格が300〜9,999,999円以外の時' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+    end
+  end
 end
