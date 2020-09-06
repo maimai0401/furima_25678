610c278fa1d0bfcd16ecf0477f68e8ef2f8f2b0c
maimai0401 <64880970+maimai0401@users.noreply.github.com>
2020-09-06 18:21:49 +0900

    カラム名修正

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_031039) do
     t.integer "status_id", null: false
     t.integer "postage_id", null: false
     t.integer "prefecture_id", null: false
-    t.integer "days_id", null: false
+    t.integer "day_id", null: false
     t.integer "price", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
